eeb6df7162b54345e1d541c52d1d4b8d2e1d98b0
Dominik Domagalski <dragenetdev@gmail.com>
2022-02-23 18:50:36 +0100

    Add common

--- my-inventory-backend/src/index.ts
+++ my-inventory-backend/src/index.ts
@@ -1,9 +1,11 @@
+import {TestInterface} from "my-inventory-common"
+
 export const delayMillis = (delayMs: number): Promise<void> => new Promise(resolve => setTimeout(resolve, delayMs));
 
-export const greet = (name: string): string => `Hello ${name}`
+export const greet = (name: TestInterface): string => `Hello ${name}`
 
 export const foo = async (): Promise<boolean> => {
-  console.log(greet('World'))
+  console.log(greet('Woreld'))
   await delayMillis(1000)
   console.log('done')
   return true
