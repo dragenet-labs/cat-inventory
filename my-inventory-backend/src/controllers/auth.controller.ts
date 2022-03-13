1b28709fa7dd66a2e15da70284e53fe9e7e6c654
Dominik Domagalski (DrageNet) <dragenetdev@gmail.com>
2022-03-13 20:08:00 +0100

    Validate is email already used

--- my-inventory-backend/src/controllers/auth.controller.ts
+++ my-inventory-backend/src/controllers/auth.controller.ts
@@ -2,23 +2,27 @@ import { ZodRegisterUserRequestDTO } from 'my-inventory-common/dto';
 import { parseResponse, responseOf, TypedRequestBody } from 'src/utils';
 import bcrypt from 'bcrypt';
 import { services } from 'src/services';
-import { checkIsInvitationValid } from 'src/services/invitation.service';
 import { HttpInvalidInvitationCode } from 'my-inventory-common/errors';
-import { zodUserDTO } from 'my-inventory-common/dist/dto';
+import { zodUserDTO } from 'my-inventory-common/dto';
+import { storages } from 'src/storages/prisma-postgres';
+import { HttpUserWithEmailAlreadyExist } from 'my-inventory-common/dist/errors';
 
 export const registerUser = async (data: TypedRequestBody<ZodRegisterUserRequestDTO>) => {
   const { email, password, invitationCode } = data.body;
-  const invitation = await services.invitationService.getInvitationByCode(invitationCode);
+  const [invitation, user] = await Promise.all([
+    await services.invitationService.getInvitationByCode(invitationCode),
+    await storages.userStorage.getUser({ email })
+  ]);
 
+  if (user !== null) throw new HttpUserWithEmailAlreadyExist();
   if (invitation === null) throw new HttpInvalidInvitationCode();
-  await checkIsInvitationValid(invitation);
 
   const passwordHash = await bcrypt.hash(password, parseInt(process.env.PASSWORD_HASH_ROUNDS));
 
-  const user = await services.authService.createUserWithInvitation(
+  const createdUser = await services.authService.createUserWithInvitation(
     { email, passwordHash },
     invitation
   );
 
-  return responseOf(parseResponse(user, zodUserDTO));
+  return responseOf(parseResponse(createdUser, zodUserDTO));
 };
--- my-inventory-common/src/errors/AppError.ts
+++ my-inventory-common/src/errors/AppError.ts
@@ -32,3 +32,9 @@ export class HttpInactiveInvitationCode extends HttpBadRequest {
     super(ErrorTypes.INACTIVE_INVITATION_CODE, 'Your invitation code is inactive now.');
   }
 }
+
+export class HttpUserWithEmailAlreadyExist extends HttpBadRequest {
+  constructor() {
+    super(ErrorTypes.EMAIL_ALREADY_USED, 'User with specified email already exists');
+  }
+}
