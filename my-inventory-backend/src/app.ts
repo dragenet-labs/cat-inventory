b0ab1f8ebdef1594302afa60eee5a9d854ccb5af
Dominik Domagalski (DrageNet) <dragenetdev@gmail.com>
2022-03-23 23:21:31 +0100

    Extract express config

---
+++ my-inventory-backend/src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import { DefaultErrorMiddleware, ErrorMiddleware } from 'src/middlewares';
+import methodOverride from 'method-override';
+import { appRoutes } from 'src/routes';
+import { configurePassport } from 'src/middlewares/passport';
+import cors from 'cors';
+
+const defaultMorganLogsFormat =
+  ':status :method :url HTTP/:http-version :remote-addr :res[content-length]B ":user-agent" - :response-time ms';
+
+export const app = express();
+
+app.use(methodOverride());
+app.use(morgan(defaultMorganLogsFormat));
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+configurePassport(app);
+
+app.use(appRoutes);
+
+app.use(DefaultErrorMiddleware);
+app.use(ErrorMiddleware);
--- my-inventory-backend/src/index.ts
+++ my-inventory-backend/src/index.ts
@@ -1,36 +1,14 @@
-import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
-import { DefaultErrorMiddleware, ErrorMiddleware } from 'src/middlewares';
-import methodOverride from 'method-override';
-import { appRoutes } from 'src/routes';
-import { configurePassport } from 'src/middlewares/passport';
-import cors from 'cors';
+import { app } from 'src/app';
 
-dotenv.config();
+const dotenvConfig = {
+  path: process.env.NODE_ENV === 'test' ? path.resolve(process.cwd(), '.env.test') : undefined
+};
+console.log('dotenvConfig', dotenvConfig);
+dotenv.config(dotenvConfig);
+
 const PORT = process.env.PORT;
-
-const defaultMorganLogsFormat =
-  ':status :method :url HTTP/:http-version :remote-addr :res[content-length]B ":user-agent" - :response-time ms';
-
-const app = express();
-
-app.use(methodOverride());
-app.use(morgan(defaultMorganLogsFormat));
-app.use(cors());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(bodyParser.json());
-configurePassport(app);
-
-app.use(appRoutes);
-
-app.use(DefaultErrorMiddleware);
-app.use(ErrorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
