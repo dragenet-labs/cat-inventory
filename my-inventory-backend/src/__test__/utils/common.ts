90b967015c0f961813208c284e764f8c6524f9d8
Dominik Domagalski (DrageNet) <dragenetdev@gmail.com>
2022-03-25 02:07:58 +0100

    Add cookie parser

--- my-inventory-backend/src/__test__/utils/common.ts
+++ my-inventory-backend/src/__test__/utils/common.ts
@@ -4,3 +4,36 @@ import { HttpError } from 'my-inventory-common/errors';
 export interface TestResponseWithBody<T> extends Request {
   body: T | HttpError;
 }
+interface CookieCandidate {
+  name: string;
+  value: string;
+  path?: string;
+  expires?: string;
+}
+type Cookie = Exclude<CookieCandidate, 'name'>;
+interface CookieParser {
+  [key: string]: Cookie;
+}
+export const cookieParser = (cookies: string[]): CookieParser =>
+  cookies
+    .map((cookie) => cookie.split(';'))
+    .reduce((prevCookies, cookieArr) => {
+      const cookieCandidate: CookieCandidate = cookieArr
+        .map((cookieArrItem) => cookieArrItem.split('=').map((item) => item.trim()))
+        .reduce<CookieCandidate>((prev, current) => {
+          const [name, value] = current;
+          return { ...prev, ...matchCookieParam(name, value) };
+        }, {} as CookieCandidate);
+      return { [cookieCandidate.name]: cookieCandidate, ...prevCookies };
+    }, {});
+
+const matchCookieParam = (name: string, value: string) => {
+  switch (name) {
+    case 'Path':
+      return { path: value };
+    case 'Expires':
+      return { expires: value };
+    default:
+      return { name, value };
+  }
+};
