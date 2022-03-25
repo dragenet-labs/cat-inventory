8d3b6ad9c9f33abea1964a3a2be73f6a6b60d155
Dominik Domagalski (DrageNet) <dragenetdev@gmail.com>
2022-03-25 18:27:26 +0100

    Add login test

--- my-inventory-backend/src/__test__/e2e/auth.test.ts
+++ my-inventory-backend/src/__test__/e2e/auth.test.ts
@@ -1,8 +1,9 @@
 import supertest, { SuperAgentTest } from 'supertest';
 import { createApp } from '../../app';
 import { createTestInvitation } from '../utils/invitation';
-import { registerTestUser } from '../utils/auth';
+import { loginTestUser, registerTestUser } from '../utils/auth';
 import { prismaClient } from '../../utils';
+import { cookieParser } from '../utils/cookieParser';
 
 describe('[E2E] Auth', () => {
   let request: SuperAgentTest;
@@ -17,12 +18,31 @@ describe('[E2E] Auth', () => {
     const invitation = await createTestInvitation(request);
     const res = await registerTestUser(request, invitation.body.code, email, 'changeme');
 
-    console.log('res.body', res.body);
     expect(res.statusCode).toEqual(200);
     expect(res.body.passwordHash).toBeUndefined();
 
     const dbUser = await prismaClient.user.findFirst({ where: { email } });
-
     expect(dbUser).not.toBeNull();
+  });
+
+  it('User can login', async () => {
+    const email = 'second@user.com';
+    const invitation = await createTestInvitation(request);
+    await registerTestUser(request, invitation.body.code, email, 'changeme');
+    const res = await loginTestUser(request, email, 'changeme');
+
+    expect(res.statusCode).toEqual(200);
+
+    const cookies = cookieParser(res.headers['set-cookie']);
+    const connectSid = cookies['connect.sid'];
+    expect(connectSid).not.toBeUndefined();
+
+    const dbUser = await prismaClient.user.findFirst({ where: { email } });
+    expect(dbUser).not.toBeNull();
+    const sessions = await prismaClient.session.findMany({});
+    const userSessions = sessions.filter(
+      (session) => JSON.parse(session.data).passport.user === dbUser?.id
+    );
+    expect(userSessions).not.toEqual([]);
   });
 });
