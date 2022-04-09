8558272f0c837267c3888a4b294a9535eaaf2899
Dominik Domagalski (DrageNet) <dragenetdev@gmail.com>
2022-04-09 19:18:35 +0200

    [CAI-14] Add svgr and src global mapping

--- cat-inventory-web/vite.config.ts
+++ cat-inventory-web/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import svgrPlugin from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [svgrPlugin(), react()],
+  resolve: {
+    alias: [{ find: 'src', replacement: path.resolve(__dirname, 'src') }]
+  }
+});
