4d26add729e1a3f027eafe8a98ea589eb275773d
Dominik Domagalski (DrageNet) <dragenetdev@gmail.com>
2022-04-09 23:29:04 +0200

    [CAI-14] Add react-hook-form to login form

--- cat-inventory-web/src/components/organisms/LoginForm.tsx
+++ cat-inventory-web/src/components/organisms/LoginForm.tsx
@@ -7,24 +7,57 @@ import {
   Input,
   InputProps
 } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { zodLoginRequestDTO } from 'my-inventory-common/dto';
+import { forwardRef } from 'react';
 
 export const LoginForm = (props: FlexProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm({
+    resolver: zodResolver(zodLoginRequestDTO)
+  });
+
+  const onSubmit = (values: unknown) => console.log('form output: ', values);
   return (
-    <Flex flexDir="column" w="100%" alignItems="center" {...props}>
-      <LoginFormInput id="email" type="email" mb={6}>
+    <Flex
+      as="form"
+      onSubmit={handleSubmit(onSubmit)}
+      flexDir="column"
+      w="100%"
+      alignItems="center"
+      {...props}
+    >
+      <LoginFormInput
+        id="email"
+        type="email"
+        mb={6}
+        {...register('email')}
+        error={errors.email?.message}
+      >
         Email
       </LoginFormInput>
-      <LoginFormInput id="password" type="password" mb={6}>
+      <LoginFormInput
+        id="password"
+        type="password"
+        mb={6}
+        {...register('password')}
+        error={errors.password?.message}
+      >
         Password
       </LoginFormInput>
       <Button
-        // onClick={}
+        type="submit"
         size="lg"
         w="97px"
         colorScheme="teal"
         color="white"
         bgColor="teal.300"
         _active={{ bg: 'teal.300' }}
+        isLoading={isSubmitting}
       >
         Login
       </Button>
@@ -34,11 +67,14 @@ export const LoginForm = (props: FlexProps) => {
 
 interface LoginFormInputProps extends InputProps {
   children: string;
-  error?: unknown;
+  error?: string;
 }
-const LoginFormInput = ({ children, error, ...props }: LoginFormInputProps) => (
-  <FormControl isInvalid={!!error}>
-    <Input placeholder={children} {...props} />
-    {error && <FormErrorMessage>Email is required</FormErrorMessage>}
-  </FormControl>
+
+const LoginFormInput = forwardRef<HTMLInputElement, LoginFormInputProps>(
+  ({ children, error, mt, mb, ml, mr, ...props }, ref) => (
+    <FormControl isInvalid={!!error} mt={mt} mb={mb} ml={ml} mr={mr}>
+      <Input ref={ref} placeholder={children} {...props} />
+      {error && <FormErrorMessage>{error}</FormErrorMessage>}
+    </FormControl>
+  )
 );
