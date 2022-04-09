fcb856c5e2fa363a84959f8e93ed8f86770f9f68
Dominik Domagalski (DrageNet) <dragenetdev@gmail.com>
2022-04-09 20:39:52 +0200

    [CAI-14] Add css reset

--- cat-inventory-web/src/App.tsx
+++ cat-inventory-web/src/App.tsx
@@ -1,10 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './AppRoutes';
 import { theme } from './Theme';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
+    <CSSReset />
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
