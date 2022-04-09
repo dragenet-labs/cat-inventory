ed49e837fe24d00e57a98593a39080da5a8a49b1
Dominik Domagalski (DrageNet) <dragenetdev@gmail.com>
2022-04-09 20:36:17 +0200

    [CAI-14] Add email zod validator

--- my-inventory-common/src/dto/Auth.ts
+++ my-inventory-common/src/dto/Auth.ts
@@ -1,20 +1,23 @@
 import { z } from 'zod';
 import { zodInvitationCode } from './Invitation';
 
+export const zodEmail = z.string().email({ message: 'Invalid email address' });
+export type ZodEmail = z.infer<typeof zodPassword>;
+
 export const zodPassword = z
   .string()
   .min(8, { message: 'Password must contain at least 8 characters' });
 export type ZodPassword = z.infer<typeof zodPassword>;
 
 export const zodRegisterUserRequestDTO = z.object({
-  email: z.string().email({ message: 'Invalid email address' }),
+  email: zodEmail,
   password: zodPassword,
   invitationCode: zodInvitationCode
 });
 export type ZodRegisterUserRequestDTO = z.infer<typeof zodRegisterUserRequestDTO>;
 
 export const zodLoginRequestDTO = z.object({
-  email: z.string().email({ message: 'Invalid email address' }),
+  email: zodEmail,
   password: zodPassword
 });
 export type ZodLoginRequestDTO = z.infer<typeof zodLoginRequestDTO>;
